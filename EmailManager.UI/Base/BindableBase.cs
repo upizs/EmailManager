25cff461b8bf3a4542e253499d7b8471d7b9f20b
Toms Upis <toms.upizs@gmail.com>
2022-12-20 08:34:47 +0200

    Issue nr 2. Added WPF Base classes to inherit form

---
+++ EmailManager.UI/Base/BindableBase.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace EmailManager.UI.Base
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        //a method to use whenever you implement changgable property
+        protected virtual void SetProperty<T>(ref T member, T val,
+            [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(member, val)) return;
+            member = val;
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        //whenever you do changes to one property that could influence another
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged = delegate { };
+    }
+}
---
+++ EmailManager.UI/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using EmailManager.UI.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailManager.UI.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private BindableBase _currentViewModel;
+
+        public BindableBase CurrentViewModel
+        {
+            get { return _currentViewModel; }
+            set { SetProperty(ref _currentViewModel, value); }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
